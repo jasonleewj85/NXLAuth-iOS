9efa042586e582f9bde775d2a391656818e32a93
Jason Lee <jason.weijun.lee@gmail.com>
2018-10-30 10:49:16 +0800

    Initial Commit

---
+++ NXLAuth/NXLAuth.h
@@ -0,0 +1,19 @@
+//
+//  NXLAuth.h
+//  NXLAuth
+//
+//  Created by Jason Lee on 30/10/2018.
+//  Copyright © 2018 Jaosn Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NXLAuth.
+FOUNDATION_EXPORT double NXLAuthVersionNumber;
+
+//! Project version string for NXLAuth.
+FOUNDATION_EXPORT const unsigned char NXLAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NXLAuth/PublicHeader.h>
+
+
